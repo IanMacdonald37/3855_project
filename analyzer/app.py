b06bab40d4ce037a69668ea5052f160e08f40ef5
IanMacdonald37 <imacdonald37@my.bcit.ca>
2025-03-08 18:12:33 -0800

    lab7 draft finished

---
+++ analyzer/app.py
@@ -0,0 +1,58 @@
+import logging.config
+import yaml
+import logging
+import connexion
+import json
+from pykafka import KafkaClient
+
+with open('app_conf.yml', 'r') as f:
+    CONFIG = yaml.safe_load(f.read())
+
+with open('log_conf.yml', 'r') as f:
+    LOG_CONFIG = yaml.safe_load(f.read())
+    logging.config.dictConfig(LOG_CONFIG)
+
+logger = logging.getLogger('basicLogger')
+
+def get_index(type, index):
+    client = KafkaClient(hosts=f"{CONFIG['kafka']['hostname']}:{CONFIG['kafka']['port']}")
+    topic = client.topics[CONFIG["kafka"]["topic"].encode()]
+    consumer = topic.get_simple_consumer(reset_offset_on_start=True, consumer_timeout_ms=1000)
+    counter = 0
+    for msg in consumer:
+        message = msg.value.decode("utf-8")
+        data = json.loads(message)
+        if data["type"] == type:
+            if counter == index:
+                return data["payload"], 200
+            counter += 1
+        # Look for the index requested and return the payload with 200 status code
+    return { "message": f"No message at index {index}!"}, 404
+
+def get_odo(index):
+    return get_index("odometer_report", index)
+
+def get_job(index):
+    return get_index("job_completion", index)
+
+def get_stats():
+    client = KafkaClient(hosts=CONFIG["kafka"]["hostname"])
+    topic = client.topics[CONFIG["kafka"]["topic"].encode()]
+    consumer = topic.get_simple_consumer(reset_offset_on_start=True, consumer_timeout_ms=1000)
+    odo_count = 0
+    job_count = 0
+    for msg in consumer:
+        message = msg.value.decode("utf-8")
+        data = json.loads(message)
+        if data["type"] == "odometer_report":
+            odo_count += 1
+        elif data["type"] == "job_completion":
+            job_count += 1
+
+    return { "num_odometers": odo_count, "num_jobs": job_count}, 200
+
+app = connexion.FlaskApp(__name__, specification_dir='', strict_validation=True)
+app.add_api("openapi.yaml", strict_validation=True, validate_responses=True)
+
+if __name__ == "__main__":
+    app.run(port=8120)
